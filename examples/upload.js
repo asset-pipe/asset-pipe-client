f22b4dd993721a180296f0e320f6a5ec0521e4cf
Richard Walker <digitalsadhu@gmail.com>
2017-10-17 11:56:32 +0200

    docs(examples): Update examples to include CSS

--- examples/upload.js
+++ examples/upload.js
@@ -1,21 +1,27 @@
 'use strict';
 
+const { join } = require('path');
 const Client = require('../');
 
-const moduleA = new Client({
-    buildServerUri: 'http://127.0.0.1:7100',
-});
+async function main() {
+    const moduleA = new Client({
+        buildServerUri: 'http://127.0.0.1:7100',
+    });
 
-const moduleB = new Client({
-    buildServerUri: 'http://127.0.0.1:7100',
-});
+    const moduleB = new Client({
+        buildServerUri: 'http://127.0.0.1:7100',
+    });
 
-moduleA
-    .uploadFeed(['./assets/es5/module.a.js'])
-    .then(content => console.log(content))
-    .catch(console.error);
+    const results = await Promise.all([
+        moduleA.uploadFeed([join(__dirname, './assets/es5/module.a.js')]),
+        moduleA.uploadFeed([join(__dirname, './assets/es5/module.a.css')]),
+        moduleB.uploadFeed([join(__dirname, './assets/es5/module.b.js')]),
+        moduleB.uploadFeed([join(__dirname, './assets/es5/module.b.css')]),
+    ]);
 
-moduleB
-    .uploadFeed(['./assets/es5/module.b.js'])
+    results.map(console.log);
+}
+
+main()
     .then(content => console.log(content))
     .catch(console.error);
